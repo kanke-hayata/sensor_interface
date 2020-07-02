03b59a7b9dea25d3aa37b6fe6097329d4445b02a
Bakui Chou <chou@jsk.imi.i.u-tokyo.ac.jp>
2020-07-02 13:47:59 +0900

    Add delay after the reset in lepton thermal sensor

--- lepton_thermal_sensor/src/sensor_handler.cpp
+++ lepton_thermal_sensor/src/sensor_handler.cpp
@@ -52,6 +52,7 @@ void SensorHandler::reboot()
   Lepton::I2C::reboot();
   usleep(750000);
   Lepton::SPI::openPort(spi_dev_, spi_speed_);
+  sleep(3); // wait for 3 seconds
 }
 
 bool SensorHandler::grabImage(cv::Mat &temp_map, double& timestamp)
